01d3eb73156866ceb976fa3b2d82b9567a7a4285
Ryan K Burns <ryankburns@gmail.com>
2020-10-21 17:59:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
     html = Nokogiri::HTML(open(profile_url))
     students_info = {}
     html.css("div.social-icon-controler a").each do |student|
-      url = student.attribute("href").value
+      url = student.attribute("href")
       students_info[:twitter_url] = url if url.include?("twitter")
       students_info[:linkedin_url] = url if url.include?("linkedin")
       students_info[:github_url] = url if url.include?("github")
