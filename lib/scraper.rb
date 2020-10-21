4f6fc793b55dc026c0b0d2a4f806b522daa386b8
Ryan K Burns <ryankburns@gmail.com>
2020-10-21 17:52:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html'))
+    doc = Nokogiri::HTML(open(index_url))
       students = []
 
       doc.css('div.student-card').each do |roster|
@@ -21,15 +21,16 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open('https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html'))
       student_info = {}
-      doc.css('div.vitals-container').each do |roster|
-                student_info = {
+      container = doc.css('div.vitals-container').each do |roster|
+            container
           :twitter => roster.css("div.social-icon-container a").attribute("href").value,
           :linkedin => roster.css(""),
           :github => roster.css(""),
           :blog => roster.css(""),
+        end
           :profile_quotes => roster.css("div.profile-quote").text,
+        end
           :bio => doc.css("div.bio-content.content-holder p").text
-        }
       end
       student_info
     end
