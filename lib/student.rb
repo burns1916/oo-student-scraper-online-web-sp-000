c901bd11a4737181b9efbcaeffc7242820da70c1
Ryan K Burns <ryankburns@gmail.com>
2020-10-21 18:51:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.collect do |key, value|
-      self.send(("#{key}="), value)
+      self.new(("#{key}="), value)
     end
   end
 
